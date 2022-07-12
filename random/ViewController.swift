ece5064fbc59153050f54100f43c65a151deff2f
Pantos, Thomas <tpantos@deloitte.gr>
2022-07-13 00:58:24 +0300

    Initial Commit

---
+++ random/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  random
+//
+//  Created by Pantos, Thomas on 13/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
